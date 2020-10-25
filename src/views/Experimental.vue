492c3d733c50b6a2aa28ac030461adbca81b6ed1
Boris Muñoz <bmunoz89@users.noreply.github.com>
2020-10-25 09:45:31 -0300

    add example with experimental features

---
+++ src/views/Experimental.vue
@@ -0,0 +1,20 @@
+<template>
+  <h3 class="text">Hello world</h3>
+  <button @click="toggleColor">Press me</button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+export const color = ref<string>('red')
+
+export function toggleColor(): void {
+  color.value = (color.value === 'red') ? 'blue' : 'red'
+}
+</script>
+
+<style vars="{ color }" lang="scss" scoped>
+.text {
+  color: var(--color);
+}
+</style>
