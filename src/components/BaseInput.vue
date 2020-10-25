875492f75525e4647cdf2142d3af41da862012c0
Boris Muñoz <bmunoz89@users.noreply.github.com>
2020-10-25 00:09:56 -0300

    add vue base input example

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <label>{{ label }}</label>
+  <input
+    v-bind="{
+      ...$attrs,
+      onInput,
+    }"
+    :value="modelValue"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+type $InputEvent = InputEvent & { target: HTMLInputElement }
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+  },
+  setup(_props, { emit }) {
+    const onInput = ($event: $InputEvent) => {
+      emit('update:modelValue', $event.target.value)
+    }
+
+    return { onInput }
+  },
+})
+</script>
---
+++ src/views/Input.vue
@@ -0,0 +1,25 @@
+<template>
+  <BaseInput v-model="email" label="Email:" type="email" @blur="email = 'blurrr@its.cold'" />
+  <pre>{{ email }}</pre>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import BaseInput from '@/components/BaseInput.vue'
+
+export default defineComponent({
+  name: 'Input',
+  components: {
+    BaseInput,
+  },
+  setup() {
+    const email = ref('')
+
+    return {
+      email,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
